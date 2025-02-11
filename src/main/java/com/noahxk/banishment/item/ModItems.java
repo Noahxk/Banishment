cbd30ba01b11d13926bb215f27f978ffceed80b1
Noahxk <noahxk@outlook.com>
2025-02-11 16:38:15 +1100

    Worked out logic for 10s use duration on CrucifixItem

--- src/main/java/com/noahxk/banishment/item/ModItems.java
+++ src/main/java/com/noahxk/banishment/item/ModItems.java
@@ -14,7 +14,7 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Banishment.MOD_ID);
 
     public static final DeferredItem<Item> CRUCIFIX = ITEMS.register("crucifix",
-            () -> new CrucifixItem(new Item.Properties().useItemDescriptionPrefix().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse("banishment:crucifix")))));
+            () -> new CrucifixItem(new Item.Properties().useItemDescriptionPrefix().stacksTo(1).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse("banishment:crucifix")))));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/com/noahxk/banishment/item/custom/CrucifixItem.java
+++ src/main/java/com/noahxk/banishment/item/custom/CrucifixItem.java
@@ -1,9 +1,12 @@
 package com.noahxk.banishment.item.custom;
 
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class CrucifixItem extends Item {
     public CrucifixItem(Properties properties) {
@@ -11,10 +14,42 @@ public class CrucifixItem extends Item {
     }
 
     @Override
-    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity interactionTarget, InteractionHand usedHand) {
 
-        System.out.println(entity.getName());
+        player.startUsingItem(usedHand);
 
-        return super.onLeftClickEntity(stack, player, entity);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack, LivingEntity entity) {
+        return 200;
+    }
+
+    @Override
+    public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int remainingUseDuration) {
+        if(!level.isClientSide()) {
+            System.out.println(remainingUseDuration);
+        }
+
+        super.onUseTick(level, livingEntity, stack, remainingUseDuration);
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
+        if(!level.isClientSide()) {
+            System.out.println("Used item");
+        }
+
+        return super.finishUsingItem(stack, level, livingEntity);
+    }
+
+    @Override
+    public boolean releaseUsing(ItemStack stack, Level level, LivingEntity entity, int timeLeft) {
+        if(!level.isClientSide()) {
+            System.out.println("Released early");
+        }
+
+        return super.releaseUsing(stack, level, entity, timeLeft);
     }
 }
